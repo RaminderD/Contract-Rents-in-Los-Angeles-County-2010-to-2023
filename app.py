90fe84903f78b77db49a1549760789cb2347c25b
raminder singh dubb <ramindersw07@gmail.com>
2025-08-15 15:18:05 -0700

    Update app.py

--- app.py
+++ app.py
@@ -14,67 +14,270 @@ import json
 from copy import deepcopy
 import os
 
-# Initialize the Dash app
-app = dash.Dash(__name__)
+assets_path = "assets/"
 
-# Sample data
-categories = ['A', 'B', 'C', 'D']
-values1 = np.random.randint(10, 100, size=4)
-values2 = np.random.randint(10, 100, size=4)
+data_path = "masterfiles/"
 
-# App layout
-app.layout = html.Div([
-    dcc.Graph(id='bar-chart'),
-    dcc.Dropdown(
-        id='chart-type-dropdown',
-        options=[
-            {'label': 'Grouped', 'value': 'group'},
-            {'label': 'Stacked', 'value': 'stack'}
-        ],
-        value='group',  # Default value
-        clearable=False,
-        style={'width': '50%'}
-    ),
-    dcc.Store(id='bar-data', data={'categories': categories, 'values1': list(values1), 'values2': list(values2)})
+
+# ------------ UTILITY FUNCTIONS ------------ #
+# Function for collecting all data into a dictionary stratified by Year and Place (in that order)
+def place_data(label_ID, path = data_path):
+    """
+    Collects info on all places across all years by ACS ID into a dictionary.
+    You can subset this dictionary as
+    dictionary[Year][Place]
+    in order to obtain values for a specific place in a specific year.
+    """
+    place_data_dict = dict()
+
+    files = [file for file in sorted(os.listdir(path)) if label_ID in file]
+    years = []
+    for file in files:
+        date = file.split('_', 3)[-1]
+        date = date.split('.csv')[0]
+        years.append(int(date))
+
+    for file, year in zip(files, years):
+        filepath = path + file
+        df = pd.read_csv(filepath)
+
+        placeholder_dict = dict()
+        for place in df['PLACE'].unique().tolist():
+            mask = df.PLACE == place
+            placeholder_dict[place] = df[mask]
+
+        place_data_dict[year] = deepcopy(placeholder_dict)
+
+    return place_data_dict
+
+
+# Function for creating a dictionary where the years (keys) hold lists of places tabulated for that year
+def places_year_dict(label_ID, path = data_path):
+    """
+    Some places in Los Angeles County don't have any information for certain years.
+    Using the cleaned data, we extract the names of places which do have data for a
+    given year.
+    """
+    year_data_dict = dict()
+
+    files = [file for file in sorted(os.listdir(path)) if label_ID in file]
+    for file in files:
+        file_path = path + file
+        df = pd.read_csv(file_path)
+        places = df['PLACE'].unique().tolist()
+        year = df.at[0, 'YEAR']
+        year_data_dict[year] = places
+
+    return year_data_dict
+
+# ------------ CONTAINERS AND STRINGS------------ #
+
+# Container for geospatial choropleth map
+geodata_map = html.Div([
+    dcc.Graph(
+        id = "chloropleth_map",
+        config={'modeBarButtonsToRemove': ['pan2d', 'lasso2d', 'select2d', 'resetview'],
+                'displaylogo': False
+               },
+    )
 ])
 
-# Client-side callback (JavaScript function)
-app.clientside_callback(
-    """
-    function(chartType, data) {
-        var categories = data.categories;
-        var values1 = data.values1;
-        var values2 = data.values2;
+# Container for rent plot
+geodata_plot = html.Div([
+    dcc.Graph(
+        id = "rent_plot",
+        config={'modeBarButtonsToRemove': ['pan2d', 'lasso2d', 'select2d', 'resetview'],
+                'displaylogo': False
+               },
+    )
+])
 
-        var barmode = chartType === 'stack' ? 'stack' : 'group';
 
-        return {
-            'data': [
-                {
-                    'x': categories,
-                    'y': values1,
-                    'type': 'bar',
-                    'name': 'Series 1'
-                },
-                {
-                    'x': categories,
-                    'y': values2,
-                    'type': 'bar',
-                    'name': 'Series 2'
-                }
-            ],
-            'layout': {
-                'title': `Bar Chart (${chartType === 'stack' ? 'Stacked' : 'Grouped'})`,
-                'barmode': barmode
-            }
-        };
-    }
-    """,
-    Output('bar-chart', 'figure'),
-    [Input('chart-type-dropdown', 'value')],
-    [Input('bar-data', 'data')]
-)
 
-# Run the app
+# Footer string
+footer_string = """
+### <b style='color:#800000;'>Information</b>
+
+This interactive website allows you to view the median, 25th percentile, and 75th percentile contract rents for census tracts across various cities in Los Angeles county. <br>
+
+Use the dropdowns to choose a city of interest and a year of interest. <br>
+
+Hover over the map to view information on the median, 25th percentile, and 75th percentile contract rents for census tracts in the selected city during the selected year. <br>
+
+Click on a census tract to visualize changes in its median contract rent over time in the plot. Hover over points in the plot to view additional information on the median, 25th percentile,
+and 75th percentile contract rents for the selected census tract.
+
+<hr style="height:2px; border-width:0; color:#212122; background-color:#212122">
+
+### <b style='color:#800000;'>Notes</b>
+1. Contract rent, per the <u style='color:#800000;'><a href="https://www2.census.gov/programs-surveys/acs/methodology/design_and_methodology/2024/acs_design_methodology_report_2024.pdf">December 2024 American Community Survey and Puerto Rico Community Survey Design and Methodology</a></u>, is defined as <br>
+
+   <blockquote> <q> ...the monthly rent agreed to or contracted for, regardless of any furnishings, utilities, fees, meals, or services that may be included.</q> (Chapter 6) </blockquote>
+
+   Thus, <ul>
+   <li> The <b style='color:#800000;'>median contract rent</b> represents contract rent where 50% of all contract rents in a census tract are lower than this median, </li>
+   <li> The <b style='color:#B22222;'>25% percentile contract rent</b> represents contract rent where 25% of all contract rents in a census tract are lower than this 25th percentile</li>
+   <li> The <b style='color:#B22222;'>75% percentile contract rent</b> represents contract rent where 75% of all contract rents in a census tract are lower than this 75th percentile</li>
+   </ul>
+
+2. Data for contract rents were taken from the United States Census Bureau <u style='color:#800000;'><a href="https://www.census.gov/programs-surveys/acs.html">American Community Survey</a></u> (ACS codes B25057, B25058, and B25059).
+3. Redistricting over the years affects the availability of some census tracts in certain cities. Unavailability of data for certain census tracts during select years may affect whether or not census tracts are displayed on the map. For these reasons, some census tracts and their data may only be available for a partial range of years.
+4. For data years 2014 and prior, the American Community Survey caps the imputation of contract rents at $2000. For data years 2015 and later, the American Community Survey caps the imputation of contract rents at &#36;3500. As a result, some data on select census tracts may be unavailable in virtue of being higher than those permissible by these thresholds.
+
+### <b style='color:#800000;'>Disclaimer</b>
+
+This tool is developed for illustrative purposes. This tool is constructed with the assistance of the United States Census Bureau’s American Community Survey data.
+Survey data is based on individuals’ voluntary participation in questionnaires. The creator is not liable for any missing, inaccurate, or incorrect data. This tool
+is not affiliated with, nor endorsed by, the government of the United States.
+
+### <b style='color:#800000;'>Appreciation</b>
+Thank you to <u style='color:#800000;'><a href="https://www.wearelbre.org/">Long Beach Residents Empowered (LiBRE)</a></u> for providing the opportunity to work on this project.
+
+### <b style='color:#800000;'>Author Information</b>
+Raminder Singh Dubb <br>
+GitHub — <u style='color:#800000;'><a href=""> TO BE UPDATED </a></u>
+
+© 2025 Raminder Singh Dubb
+"""
+
+# ------------ Initialization ------------ #
+masterfile_place_year_dict = places_year_dict('contract_rent_masterfile')
+masterfile_place_data = place_data('contract_rent_masterfile')
+
+# ------------ Colors ------------ #
+Cream_color = '#FAE8E0'
+SnowWhite_color = '#F5FEFD'
+AlabasterWhite_color = '#FEF9F3'
+LightBrown_color = '#F7F2EE'
+Rose_color = '#FF7F7F'
+MaroonRed_color = '#800000'
+SinopiaRed_color = '#C0451C'
+Teal_color = '#2A9D8F'
+ObsidianBlack_color = '#020403'
+CherryRed_color = '#E3242B'
+
+
+# ------------ APP ------------ #
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SIMPLEX])
+
+app.layout = dbc.Container([
+    # ------------ Title ------------ #
+    html.Div([
+        html.B("Rents in Los Angeles County")
+    ], style = {'display': 'block',
+                'color': MaroonRed_color,
+                'margin': '0.2em 0',
+                'padding': '0px 0px 0px 0px', # Numbers represent spacing for the top, right, bottom, and left (in that order)
+                'font-family': 'Trebuchet MS, sans-serif',
+                'font-size': '220.0%'
+               }
+            ),
+    # ------------ Subtitle ------------ #
+    html.Div([
+        html.P("Median, 25th Percentile, and 75th Percentile Contract Rents for Census Tracts across Cities and Census-Designated Places in Los Angeles County, 2010 to 2023")
+    ], style = {'display': 'block',
+                'color': ObsidianBlack_color,
+                'margin': '-0.5em 0',
+                'padding': '0px 0px 0px 0px',
+                'font-family': 'Trebuchet MS, sans-serif',
+                'font-size': '105.0%'
+               }
+            ),
+    # ------------ Horizontal line rule ------------ #
+    html.Div([
+        html.Hr()
+    ], style = {'display': 'block',
+                'height': '1px',
+                'border': 0,
+                'margin': '-0.9em 0',
+                'padding': 0
+               }
+            ),
+    # ------------ Labels for dropdowns (discarded) ------------ #
+
+    # ------------ Dropdowns ------------ #
+    html.Div([
+        html.Div([
+            dcc.Dropdown(id='place-dropdown',
+                         placeholder='Select a place',
+                         options=[{'label': p, 'value': p} for p in masterfile_place_year_dict[2023]],
+                         value='Long Beach',
+                         clearable=False
+                        )
+        ], style = {'display': 'inline-block',
+                    'margin': '0 0',
+                    'padding': '0px 15px 0px 0px',
+                    'width': '22.5%'
+                   }
+                ),
+        html.Div([
+            dcc.Dropdown(id='year-dropdown',
+                         placeholder='Select a year',
+                         clearable=False
+                        )
+        ], style = {'display': 'inline-block',
+                    'margin': '0 0',
+                    'padding': '30px 15px 0px 0px',
+                    'width': '12.5%',
+                   }
+                ),
+        html.Div([
+            dcc.Dropdown(id='census-tract-dropdown',
+                         placeholder = 'Click on a census tract in the map',
+                         clearable=True
+                        )
+        ], style = {'display': 'inline-block',
+                    'padding': '0px 30px 0px 0px',
+                    'margin': '0 0',
+                    'width': '30.0%'
+                   }
+                ),
+    ]
+            ),
+    # ------------ Spatial map with plot ------------ #
+    html.Div([
+            dbc.Row([
+            dbc.Col([
+                dbc.Card([
+                    dbc.CardHeader(id = "map-title",
+                                   style = {'background-color': MaroonRed_color,
+                                            'color': '#FFFFFF'}
+                                  ),
+                    dbc.CardBody([geodata_map],
+                                 style = {'background-color': AlabasterWhite_color}
+                                )
+                ])
+            ]),
+            dbc.Col([
+                dbc.Card([
+                    dbc.CardHeader(id = "plot-title",
+                                   style = {'background-color': Teal_color,
+                                            'color': '#FFFFFF'}
+                                  ),
+                    dbc.CardBody([geodata_plot],
+                                 style = {'background-color': AlabasterWhite_color}
+                                )
+                ])
+            ])
+        ], align='center', justify='center'
+               )
+    ], style = {
+                'padding': '10px 0px 20px 0px',
+               }
+            ),
+    # ------------ Footer ------------ #
+    html.Div([
+        fmc.FefferyMarkdown(markdownStr    = footer_string,
+                            renderHtml     = True,
+                            style          = {'background': LightBrown_color,
+                                              'margin-top': '1em'
+                                             }
+                           )
+    ]
+            )
+], style = {'background-color': LightBrown_color, "padding": "0px 0px 20px 0px",})
+
+
+# ------------ EXECUTE THE APP ------------ #
 if __name__ == '__main__':
     app.run(debug=False)
